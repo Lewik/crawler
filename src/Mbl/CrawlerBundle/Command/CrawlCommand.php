2480ac321b8d7e2c55be04351ec45f10d0cf5264
Lev Shagalov <lewik@interprogma.ru>
2014-11-18 13:02:08 +0300

    common command

---
+++ src/Mbl/CrawlerBundle/Command/CrawlCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace Mbl\CrawlerBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/** Class CrawlCommand */
+class CrawlCommand extends Command
+{
+    /**
+     *
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('crawler:crawl')
+            ->setDescription('Crawler crawl!')
+//            ->addArgument(
+//                'name',
+//                InputArgument::OPTIONAL,
+//                'Who do you want to greet?'
+//            )
+//            ->addOption(
+//                'yell',
+//                null,
+//                InputOption::VALUE_NONE,
+//                'If set, the task will yell in uppercase letters'
+//            )
+        ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $output->writeln('asd');
+    }
+}
